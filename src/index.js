281f65b18f03ad6206ec7b648352efc13d8735d4
mkriventsev <makslits@gmail.com>
2020-06-27 17:45:11 +0200

    let's go

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HomeScreen from './screens/HomeScreen';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomeScreen />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/screens/HomeScreen/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './styles.scss'
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <div>
+               <h1 className='header'>My Game</h1>
+
+            </div>
+        )
+    }
+}
